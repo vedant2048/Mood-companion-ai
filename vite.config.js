892b3139c91a222d87e859bf6bb5ef71a86ce661
vedant2048 <vedantsinghh2048@gmail.com>
2026-02-24 10:37:20 +0530

    vercel

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Mood-companion-ai/",  
+  
+   
 
   plugins: [
     react(),
     tailwindcss(),
     tsconfigPaths(),
   ],
+  base: "/",
 });
